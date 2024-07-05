00e6da02c3e95f1ab899ce919403f451e238ccc7
Marvel Collin <marvelcollin7@gmail.com>
2024-07-05 19:30:17 +0700

    feat: add hour server

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -4,21 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Models\Permission;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+
+// Create the Permission record with the specified 'created_at' time
+
 
 class PermissionController extends Controller
 {
     public function show(){
         $permissions = Permission::all();
-
+        
         return view('permission', compact('permissions'));
     }
-
+    
     public function create(Request $request){
+        $createdAt = Carbon::now()->addHours(14);
+
         Permission::create([
             'trainee_number' => $request->trainee_number,
-            'reason' => $request->reason
+            'reason' => $request->reason,
+            'created_at' => $createdAt,
         ]);
-
+        
         // dd($request->trainee_number);
         return redirect()->back();
     }
